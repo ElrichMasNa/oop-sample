5b8fba47cd23e9d31353624f7f482d37489246ac
alumnos <u20191c464@upc.edu.pe>
2025-09-05 17:58:38 -0500

    feat(order): implement supplier and purchase order creation with item management

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ACME.OOP.Procurement.Domain.Model.Aggregates;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.Aggregates;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
+
+var supplierAddress = new Address(street: "Main St", number: "1213", city:"Anytown", stateOrRegion: "Ca",
+                                    postalCode:"12345", country:"USA");
+
+var supplier = new Supplier("SUP123", "Acme Supplies", supplierAddress);
+
+var purchaseOrder = new PurchaseOrder("PO12345", new SupplierId(supplier.Identifier), DateTime.Now, "USD");
+
+purchaseOrder.AddItem(ProductId.New(), 10, 15.00m);
+purchaseOrder.AddItem(ProductId.New(), 5, 25.00m);
+Console.WriteLine($"Order Total: {purchaseOrder.CalculateOrderTotal()}");
