a0cf42add5442c6826fa0e2e751a9ef66d5472f1
alumnos <u20191c464@upc.edu.pe>
2025-09-05 17:10:39 -0500

    feat(product): enhance ProductId and update PurchaseOrder structure

--- ACME.OOP/Procurement/Domain/Model/Aggregates/PurchaseOrder.cs
+++ ACME.OOP/Procurement/Domain/Model/Aggregates/PurchaseOrder.cs
@@ -5,5 +5,11 @@ namespace ACME.OOP.Procurement.Domain.Model.Aggregates;
 public class PurchaseOrder(string orderNumber, Supplier supplierID, 
     DateTime orderDate, string currency)
 {
-        
+        private List<PurchaseOrderItem> _items = new();
+        public Supplier Supplier { get; } = supplierID ?? throw new ArgumentNullException(nameof(supplierID), "supplierID cannot be null");
+        public OrderNumber OrderNumber { get; } = orderNumber ?? throw new ArgumentNullException(nameof(orderNumber), "orderNumber cannot be null");
+        public DateTime OrderDate { get; } = orderDate;
+        public string Currency { get; } = string.IsNullOrWhiteSpace(currency) || currency.Length! = 3 ? throw new ArgumentNullException(nameof(currency)) : currency;
+
+        public IReadOnlyList<PurchaseOrderItem> Items => _items.AsReadOnly();
 }
--- ACME.OOP/Procurement/Domain/Model/ValueObjects/ProductId.cs
+++ ACME.OOP/Procurement/Domain/Model/ValueObjects/ProductId.cs
@@ -1,3 +1,16 @@
 namespace ACME.OOP.Procurement.Domain.Model.ValueObjects;
 
-public record ProductId();
+public record ProductId
+{
+    public Guid Id{get; init; }
+    public ProductId(Guid id)
+    {
+        if (id == Guid.Empty)
+            throw new ArgumentException("ProductId cannot be empty", nameof(id));
+        Id = id;
+    }
+    
+    public static ProductId New() => new(Guid.NewGuid());
+    
+    public override string ToString() => Id.ToString();
+}
